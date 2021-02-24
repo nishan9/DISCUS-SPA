89d16e95b6ae16e43361ed3493b8c5687e322bf6
Nishan Fernando <nfm24@sussex.ac.uk>
2021-02-24 15:03:33 +0000

    Added download support for events

--- src/ViewEventEntity.tsx
+++ src/ViewEventEntity.tsx
@@ -3,13 +3,22 @@ import { Avatar, Box, Grid, Typography } from '@material-ui/core';
 import React, { useContext, useEffect, useState } from 'react'
 import { EditEventContext } from './context/EditEventContext';
 import EventAttendance from './models/EventAttendance';
+import { CSVLink, CSVDownload } from "react-csv";
 
 function ViewEventEntity(props : any) {
     const EventContext = useContext(EditEventContext)
     const [accessToken, setAccessToken] = useState(''); 
     const Auth0 = useAuth0();
     const [eventAttendance,setEventAttendance ] = useState<EventAttendance>(); 
-    
+    const headers = [
+        { label: ' Name', key: 'name' },
+        { label: 'Email Address', key: 'email' },
+        { label: 'Expertise', key: 'user_metadata.expertise' },
+        { label: 'Interests', key: 'user_metadata.interest' },
+
+      ];
+
+
     useEffect(() => {
         getEventInfo(); 
         getEventAttendance(); 
@@ -41,32 +50,32 @@ function ViewEventEntity(props : any) {
         <div>
             <p>Event ID : {EventContext.event.id} 
             
-       
+            {eventAttendance ? <CSVLink filename={EventContext.event.title} headers = {headers} data={eventAttendance.users}>Download me</CSVLink>  : ""}
+
             <Grid container justify="center">
-                            <Box>
-                                <Typography variant={"h3"}>{EventContext.event.title}</Typography>
-                            </Box>
-                        </Grid>
-                            <Box>
-                                <Typography variant="body2">{EventContext.event.type}</Typography>
-                                <Typography variant="body2">{EventContext.event.url}</Typography>
-                                <Typography variant="body2"> Is DISCUS : {EventContext.event.isDISCUS.toString()}</Typography>
-                                <Typography>{EventContext.event.description}</Typography>
-                                <Typography>Start = {EventContext.event.dateTime}</Typography>
-                                <Typography> Finish = {EventContext.event.finishedDateTime}</Typography>
-                                <Typography> Tags - {EventContext.event.tags}</Typography>
-                            </Box>  
-            <Typography variant="h2"> Attendees</Typography>            
-           
-             {eventAttendance?.users.map (e => 
-                     <Grid container>  
-                         <Avatar alt="Cindy Baker" src={e.picture} />
-                         <Typography>{e.name}</Typography>
-                     </Grid>
+                <Box>
+                    <Typography variant={"h3"}>{EventContext.event.title}</Typography>
+                </Box>
+                </Grid>
+                <Box>
+                    <Typography variant="body2">{EventContext.event.type}</Typography>
+                    <Typography variant="body2">{EventContext.event.url}</Typography>
+                    <Typography variant="body2"> Is DISCUS : {EventContext.event.isDISCUS.toString()}</Typography>
+                    <Typography>{EventContext.event.description}</Typography>
+                    <Typography>Start = {EventContext.event.dateTime}</Typography>
+                    <Typography> Finish = {EventContext.event.finishedDateTime}</Typography>
+                    <Typography> Tags - {EventContext.event.tags}</Typography>
+                </Box>  
+                <Typography variant="h2"> Attendees</Typography>            
+                {eventAttendance?.users.map (e => 
+                    <Grid container>  
+                        <Avatar alt="Cindy Baker" src={e.picture} />
+                        <Typography>{e.name}</Typography>
+                    </Grid>
             )}
 
             <Box>
-            Total users {eventAttendance?.total}
+                Total users {eventAttendance?.total}
             </Box>
             
             </p>
--- src/models/EventAttendance.tsx
+++ src/models/EventAttendance.tsx
@@ -1,12 +1,20 @@
-
-
-interface EventAttendance{
+interface EventAttendance
+{
     "users" : EventUser[], 
     "total" : number, 
+    "user_metadata": ExpertiseInterests, 
 }
 
-interface EventUser{
+interface EventUser
+{
     "name" : string,
-    "picture" : string
+    "picture" : string, 
+    "email" : string,
+}
+
+interface ExpertiseInterests
+{
+    "expertise" : string[], 
+    "interest" : string[],
 }
 export default EventAttendance;  
