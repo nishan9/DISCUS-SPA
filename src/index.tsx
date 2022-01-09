b0039c77e1d49eb33a3c5135ce26539c439c69f1
nishan9 <47658117+nishan9@users.noreply.github.com>
2022-01-09 16:41:54 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,7 @@ function Wrapper(){
   return(
     <Auth0Provider
     domain="discus.eu.auth0.com"
-    clientId="gfoRzpVs5QAOFNuw08R3Vlf8nqaA6c5G"
+    clientId="..."
     audience="https://discus.eu.auth0.com/api/v2/"
     redirectUri={window.location.origin}
   >
